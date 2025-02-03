8332ae0e0f082857865a71d023410ef96db03403
Konstantin <snorchello@gmail.com>
2025-02-04 02:37:33 +0700

    Added solvation

--- 12_Integer_to_Roman.go
+++ 12_Integer_to_Roman.go
@@ -13,56 +13,11 @@
 
 package main
 
-import (
-	"strings"
-)
-
 func intToRoman(num int) string {
-	num1 := num / 1000
-	num2 := num / 100 % 10
-	num3 := num / 10 % 10
-	num4 := num % 10
+	num1 := []string{"", "M", "MM", "MMM", "MMMM", "MMMMM"}
+	num2 := []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+	num3 := []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
+	num4 := []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
 
-	s1 := strings.Repeat("M", num1)
-	s2, s3, s4 := "", "", ""
-
-	switch num2 {
-	case 4:
-		s2 = "CD"
-	case 9:
-		s2 = "CM"
-	default:
-		if num2 > 4 {
-			s2 = "D"
-			num2 = num2 - 5
-		}
-		s2 = s2 + strings.Repeat("C", num2)
-	}
-
-	switch num3 {
-	case 4:
-		s3 = "XL"
-	case 9:
-		s3 = "XC"
-	default:
-		if num3 > 4 {
-			s3 = "L"
-			num3 = num3 - 5
-		}
-		s3 = s3 + strings.Repeat("X", num3)
-	}
-
-	switch num4 {
-	case 4:
-		s4 = "IV"
-	case 9:
-		s4 = "IX"
-	default:
-		if num4 > 4 {
-			s4 = "V"
-			num4 = num4 - 5
-		}
-		s4 = s4 + strings.Repeat("I", num4)
-	}
-	return s1 + s2 + s3 + s4
+	return num1[num/1000] + num2[(num%1000)/100] + num3[(num%100)/10] + num4[num%10]
 }
